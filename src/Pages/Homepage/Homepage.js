e66d89c5e3ba4574cdfbf8f363c669ba86ec1a27
Jason Schneider <jasonmschneider@outlook.com>
2021-02-13 20:45:08 -0500

    Formatted home page properly

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -6,10 +6,12 @@ const Homepage = () => {
     return(
         <div className={classes.mainContainer}>
             <div className={classes.Header}>
-                <h1>Hi, I'm Jason.</h1>
+                <h1>Hi. I'm Jason and this is my website.</h1>
             </div>
             <img className={classes.Photo} src={homeimage} alt={'Failed to load'}/>
-            <div className={classes.Description}>Description</div>
+            <div className={classes.Description}><p>I am currently employed as a data scientist 
+                at Rattlehub Digital where I work on predictive insights models for financial advisors
+                as well as digital engagement for collection of estate data. </p> </div>
         </div>
     )
 }
