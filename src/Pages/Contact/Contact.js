bd1e1bff4b06d706d5af5a035c20759ecd2535dd
Jason Schneider <jasonmschneider@outlook.com>
2021-02-15 13:22:46 -0500

    Added input box to Contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import classes from './Contact.module.css';
 
 const Contact = () => {
     return(
-        <div>
+        <div className={classes.flexContainer}>
             <h1>Contact me via e-mail</h1>
             <p>This doesn't actually work yet</p>
-            
+            <input placeholder='First Name' />
         </div>
     )
 };
