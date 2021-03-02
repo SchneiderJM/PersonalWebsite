be5ea4f8301a8d1d12ff209d5bd4fd6d40ea1325
Jason Schneider <jasonmschneider@outlook.com>
2021-03-01 21:14:59 -0500

    Fixed formatting of the Project Cards

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -4,20 +4,34 @@ import {Link} from 'react-router-dom';
 
 const ProjectCard = (props) => {
     const [cardChildren,setCardChildren] = useState(<div>Empty Card</div>)
+    //useState and useEffect may seem unnecessary here, it's so I can change the order of the items
+    //depending on whether the card is right-oriented or left-oriented
     useEffect(()=>{
-        if (props.orientLeft){
+        /*if (props.orientLeft){
             setCardChildren([
                 <Link key={props.keyValue}to={props.Link}><h1 className={classes.ProjectTitle}>{props.Name}</h1></Link>,
                 <Link key={props.keyValue+1} to={props.Link}><img className={classes.ProjectLogo} src={props.Logo} alt='Logo Not Found' /></Link>,
                 <Link key={props.keyValue+2} to={props.Link}><p className={classes.ProjectDescription}>{props.Description}</p></Link>,
                 <Link key={props.keyValue+3} to={props.Link}><div></div></Link>
             ])
-        } else {
+        }*/
+        if (props.orientLeft){
+            setCardChildren([
+                <div key={props.keyValue} className={classes.leftGrid}>
+                    <Link to={props.Link}><h1 className={classes.ProjectTitle}>{props.Name}</h1></Link>
+                    <Link to={props.Link}><p className={classes.ProjectDescription}>{props.Description}</p></Link>,
+                </div>,
+                <Link key={props.keyValue+1} to={props.Link}><img className={classes.ProjectLogo} src={props.Logo} alt='Logo Not Found' /></Link>,
+
+            ])
+        }
+        else {
             setCardChildren([
                 <Link key={props.keyValue} to={props.Link}><img className={classes.ProjectLogo} src={props.Logo} alt='Logo Not Found' /></Link>,
-                <Link key={props.keyValue+1} to={props.Link}><h1 className={classes.ProjectTitle}>{props.Name}</h1></Link>,
-                <Link key={props.keyValue+2} to={props.Link}><div></div></Link>,
-                <Link key={props.keyValue+3} to={props.Link}><p className={classes.ProjectDescription}>{props.Description}</p></Link>
+                <div key={props.keyValue+1} className={classes.rightGrid}>
+                    <Link to={props.Link}><h1 className={classes.ProjectTitle}>{props.Name}</h1></Link>
+                    <Link to={props.Link}><p className={classes.ProjectDescription}>{props.Description}</p></Link>
+                </div>
             ])
         }
     },[props.Description,props.Logo,props.Name,props.keyValue,props.orientLeft,props.Link]);
